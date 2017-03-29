0d31c658d3e0ea84780922c6fa5dab2010b06409
John Nowinsky <armthepit@armthepit.com>
2017-03-29 12:11:10 -0500

    move store scripts under assets and rename folder scripts

--- assets/scripts/db_connect.php
+++ assets/scripts/db_connect.php
@@ -4,12 +4,12 @@
 	$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
 
 	if(!$connection){
-		echo "Failed to connect database" . die(mysqli_error($connection));
+		echo "Failed to connect to database" . die(mysqli_error($connection));
 	}
 
 	$dbselect = mysqli_select_db($connection, DB_DATABASE);
 
 	if(!$dbselect){
-		echo "Failed to Select database" . die(mysqli_error($connection));
+		echo "Failed to open database" . die(mysqli_error($connection));
 	}
 ?>
